9fff02d556750f5eaf374061cfa75ca42ea512f2
Ville-Veikko Saari <ville-veikko.saari@cgi.com>
2018-07-04 18:45:24 +0300

    hsl-component-library added, but at what cost..

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
+import {Theme, ToggleButton} from 'hsl-shared-components'
 import DevTools from 'mobx-react-devtools'
 import * as React from 'react'
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import {ThemeProvider} from 'styled-components'
 import './App.css'
 import Login from './Login'
 import Map from './Map'
@@ -12,18 +14,21 @@ class App extends React.Component<any, any> {
 
     public render(): any {
         return (
-            <Router>
-                <div>
-                    <DevTools/>
-                    <Map/>
-                    <nav>
-                        <Link to={'/'}>/</Link>&nbsp;
-                        <Link to={'/login'}>Login</Link>
-                    </nav>
-                    <Route exact={true} path='/' rootPath={rootPath}/>
-                    <Route path='/login/' component={Login} rootPath={rootPath}/>
-                </div>
-            </Router>
+            <ThemeProvider theme={Theme}>
+                <Router>
+                    <div>
+                        <ToggleButton title='Toggle me!' />
+                        <DevTools/>
+                        <Map/>
+                        <nav>
+                            <Link to={'/'}>/</Link>&nbsp;
+                            <Link to={'/login'}>Login</Link>
+                        </nav>
+                        <Route exact={true} path='/' rootPath={rootPath}/>
+                        <Route path='/login/' component={Login} rootPath={rootPath}/>
+                    </div>
+                </Router>
+            </ThemeProvider>
         )
     }
 }
